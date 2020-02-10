90e09d5aa60b4967826f1be96d2b1d5fdfdbf371
asuthamm <asuthamm@gmail.com>
2020-02-10 17:40:41 -0500

    changes made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:persist]
     def index
         @users = User.all  
         render json: @users
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        byebug
+        # byebug
         if @user.valid?
             wristband = encode_token({user_id: @user.id})
             render json: {user: UserSerializer.new(@user), token: wristband}
@@ -19,6 +20,21 @@ class UsersController < ApplicationController
             render json: {error: "Try again"}
         end
     end
+
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+          wristband = encode_token({user_id: @user.id})
+          render json: {user: UserSerializer.new(@user), token: wristband}
+        else
+          render json: {error: "Invalid username or password"}
+        end
+    end
+
+    def persist
+        wristband = encode_token({user_id: @user.id})
+        render json: {user: UserSerializer.new(@user), token: wristband}
+      end
 
     private
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :todo_lists
   resources :users
+  post '/login', to: "users#login"
+  get  '/persist', to: "users#persist"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
