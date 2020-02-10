4993bc909c37cf2654359e0475897366c9a6514b
bigfishh <anniezheng.anniee@gmail.com>
2020-02-10 11:40:12 -0500

    JWT Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all  
-    render json: @users
-  end
+    def index
+        @users = User.all  
+        render json: @users
+    end
 
-  def show
-    @user = User.find(params[:id])
-    render json: {user: UserSerializer.new(@user)}
-  end
+    def show
+        @user = User.find(params[:id])
+        render json: {user: UserSerializer.new(@user)}
+    end
 
-  def create
-    @user = User.create(user_params)
-  end
+    def create
+        @user = User.create(user_params)
+        byebug
+        if @user.valid?
+            wristband = encode_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), token: wristband}
+        else
+            render json: {error: "Try again"}
+        end
+    end
 
-  private
-  def user_params
-    params.permit(:name, :username, :password, :bio, :img_url)
-  end
+    private
+    def user_params
+        params.permit(:name, :username, :password, :bio, :img_url)
+    end
 end
